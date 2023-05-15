5fb151f08c0eea0943ef939c99c68f214401c682
MozRella <amirraafatnasry@gmail.com>
2023-05-15 20:53:34 +0300

    final

--- CustomerMenu.java
+++ CustomerMenu.java
@@ -1,21 +1,17 @@
-public class CustomerMenu
-{
-    public static void start() 
-    {
+public class CustomerMenu {
+    public static void start() {
         System.out.println("01. Rent");
         System.out.println("02. Buy");
         int customerChoice = ScannerUtil.takeInteger(2, 1);
 
-        if (customerChoice == 1) 
-        {
+        if (customerChoice == 1) {
             if (Storage.rentableCarCount == 0)
                 System.out.println("There is no cars for rent");
             else
                 displayRentableCarsData();
         }
 
-        if (customerChoice == 2) 
-        {
+        if (customerChoice == 2) {
             if (Storage.sellableCarCount == 0)
                 System.out.println("There is no cars for selling");
             else
@@ -23,43 +19,49 @@ public class CustomerMenu
         }
     }
 
-    public static void displayRentableCarsData() 
-    {
-        for (int i = 0; i < Storage.rentableCarCount; i++) 
-        {
-            System.out.println();
-            System.out.println("-" + (i + 1) + "--------------------------------");
+    public static void displayRentableCarsData() {
+        if (checkForAvailableRentableCar()) {
+            for (int i = 0; i < Storage.rentableCarCount; i++) {
+                if (Storage.rentableCar[i].isAvailable()) {
+                    System.out.println();
+                    System.out.println("-" + (i + 1) + "--------------------------------");
 
-            if (Storage.rentableCar[i].isAvailable()) {
-                System.out.println("01. Car Accessibility? " + Storage.rentableCar[i].getAccessibility());
-                System.out.println("02. Body Type: " + Storage.rentableCar[i].getBodyType());
-                System.out.println("03. Fuel Type: " + Storage.rentableCar[i].getFuelType());
-                System.out.println("04. Transmission Type: " + Storage.rentableCar[i].getTransmissionType());
-                System.out.println("05. Color: " + Storage.rentableCar[i].getColor());
-                System.out.println("06. Number of Seats: " + Storage.rentableCar[i].getNumberOfSeats());
+                    System.out.println("01. Car Accessibility? " + Storage.rentableCar[i].getAccessibility());
+                    System.out.println("02. Body Type: " + Storage.rentableCar[i].getBodyType());
+                    System.out.println("03. Fuel Type: " + Storage.rentableCar[i].getFuelType());
+                    System.out.println("04. Transmission Type: " + Storage.rentableCar[i].getTransmissionType());
+                    System.out.println("05. Color: " + Storage.rentableCar[i].getColor());
+                    System.out.println("06. Number of Seats: " + Storage.rentableCar[i].getNumberOfSeats());
+                } else {
+                    System.out.println("Rented");
+                }
+
+                System.out.println("----------------------------------");
+                System.out.println();
             }
-            else
-                System.out.println("Rented");
 
-            System.out.println("----------------------------------");
-            System.out.println();
-        }
-
-        // Takes Customer Choice
-        System.out.println("Choose Number");
-        int rented = ScannerUtil.takeInteger(Storage.rentableCarCount, 1);
-        int index = (rented - 1);
-        // register
-        register();
-        // Sets the car as unavailable
-        Storage.rentableCar[index].setAvailable(false);
-        Storage.customer.setRentedCars(Storage.rentableCar[index]);
+            // Takes Customer Choice
+            System.out.println("Choose Number");
+            int rented = ScannerUtil.takeInteger(Storage.rentableCarCount, 1);
+            int index = (rented - 1);
+            // register
+            register();
+            // Sets the car as unavailable
+            Storage.rentableCar[index].setAvailable(false);
+            Storage.customer.setRentedCars(Storage.rentableCar[index]);
+        } else
+            System.out.println("NO CARS!");
     }
 
-    public static void displayBuyableCarsData() 
-    {
-        for (int i = 0; i < Storage.sellableCarCount; i++)
-        {
+    public static boolean checkForAvailableRentableCar() {
+        for (int i = 0; i < Storage.rentableCarCount; i++)
+            if (Storage.rentableCar[i].isAvailable())
+                return true;
+        return false;
+    }
+
+    public static void displayBuyableCarsData() {
+        for (int i = 0; i < Storage.sellableCarCount; i++) {
             System.out.println();
             System.out.println("-" + (i + 1) + "--------------------------------");
 
@@ -88,8 +90,7 @@ public class CustomerMenu
         Storage.customer.setBoughtCars(Storage.sellableCar[index]);
     }
 
-    public static void register()
-    {
+    public static void register() {
         String email = ScannerUtil.takeString("Email: ");
         String username = ScannerUtil.takeString("Username: ");
         String password = ScannerUtil.takeString("Password: ");
